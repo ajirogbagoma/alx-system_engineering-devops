362f9bacc5277f8e75050d32fc455c49ab12d869
ajirogbagoma <oghenetegaangela@gmail.com>
2024-06-04 05:33:17 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
 
+"""
+Querries the Reddit API and returns the total number of subscribers for a
+given subreddit.
+If an invalid subreddit is given, return 0.
+"""
+import json
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """Querries Reddit API and returns total subscribers for a given subreddit,
+       or 0 if invalid subreddit given.
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+
+    headers = {
+        "User-Agent": "holberton"
+    }
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    if response.status_code != 200:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
